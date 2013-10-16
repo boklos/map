ec25c0a626eea568fe7340a05a247786afc971be
khaled h elagamy cs student <khe2@afsconnect1.njit.edu>
2013-10-15 23:02:20 -0400

    map files

---
+++ public_html/map/markers.js
@@ -0,0 +1,272 @@
+var markers = 
+{
+  "type": "FeatureCollection",
+  "features": [
+    { "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.979828, 40.762909]},
+      "properties": {
+            'marker-color': '#FF634D',
+            'marker-symbol': 'cinema',
+            'image': "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/zigfield.jpg",
+            'url': "http://clearviewcinemas.com/ziegfeld.asp",
+            'yelp': "http://api.yelp.com/v2/business/clearview-cinemas-ziegfeld-new-york",
+            'name': "The Ziegfield",
+            'address': "141 W. 54th St. New York City, NY",
+            'rating': "Yelp rating"
+        }
+    },
+    { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [-74.004115, 40.728483]},
+        "properties": {
+
+            'marker-color': '#FF634D',
+            'marker-symbol': 'cinema',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/filmforum.jpg",
+            "url": "http://www.filmforum.org/",
+            "name": "Film Forum",
+            'yelp': "http://api.yelp.com/v2/business/film-forum-new-york",
+            "address": "209 West Houston Street. New York City, NY",
+            'rating': "Yelp rating"
+        }
+    },
+    { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [-74.003917, 40.651233]},
+        "properties": {
+
+            'marker-color': '#FFF0AA',
+            'marker-symbol': 'museum',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/Madame_Tussaud_039_s_Wax_Museum_Manhattan_museums_0.jpg",
+            "url": "http://www.madametussauds.com/",
+            "name": "Madame Tussauds",
+            "address": "234 West 42nd Street. New York City, NY",
+            'yelp': "http://api.yelp.com/v2/business/madame-tussauds-new-york-new-york",
+            'rating': "Yelp rating"
+        }
+    },
+    { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [-74.01445, 40.70317]},
+        "properties": {
+
+            'marker-color': '#FFF0AA',
+            'marker-symbol': 'museum',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/ellis.jpg",
+            "url": "http://www.ellisisland.org/",
+            "name": "Ellis Island Immigration Museum",
+            "address": "Battery Park and Liberty Island. New York City, NY",
+            'yelp': "http://api.yelp.com/v2/business/ellis-island-immigration-museum-new-york",
+            'rating': "Yelp rating"
+        }
+    },
+        { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [-73.993369, 40.734164]},
+        "properties": {
+
+            'marker-color': '#FF634D',
+            'marker-symbol': 'cinema',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/CinemaVillage2.jpg",
+            "url": "http://www.cinemavillage.com/chc/cv/",
+            "name": "Cinema Village",
+            "address": "22 E. 12th St. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/cinema-village-new-york",
+            "rating" : "Yelp rating"
+        }
+    },
+ 
+       { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [-73.98824, 40.75657]},
+        "properties": {
+
+            'marker-color': '#FF634D',
+            'marker-symbol': 'cinema',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/amc.jpg",
+            "url": "http://www.amctheatres.com/",
+            "name": "AMC Theatres",
+            "address": "234 W. 42nd St. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/amc-empire-25-new-york",
+            "rating" : "Yelp rating"
+        }
+    },
+      { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [-73.986478, 40.732997]},
+        "properties": {
+
+            'marker-color': '#BDF4A0',
+            'marker-symbol': 'bar',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/arthur-avenue-market.jpg",
+            "url": "http://www.arthurstavern.com/",
+            "name": "Arthur's Tavern",
+            "address": "57 Grove St Between 7th Ave. S and Bleecker St. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/arthurs-tavern-new-york",
+            "rating" : "Yelp rating"
+        }
+    },
+      { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [-74.00628, 40.741339]},
+        "properties": {
+
+            'marker-color': '#FFF0AA',
+            'marker-symbol': 'museum',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/Ground_Zero_Museum_Workshop_New_York_City.jpg",
+            "url": "http://www.groundzeromuseumworkshop.com/",
+            "name": "Ground Zero Museum Workshop",
+            "address": "420 West 14th St. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/ground-zero-museum-workshop-new-york",
+            "rating" : "Yelp rating"
+        }
+    },
+      { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [-73.954657, 40.734419]},
+        "properties": {
+
+            'marker-color': '#FFF0AA',
+            'marker-symbol': 'museum',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/intrepid.jpg",
+            "url": "http://www.intrepidmuseum.org/",
+            "name": "Intrepid Sea, Air & Space Museum",
+            "address": "12th Avenue & 46th Street, Pier 86. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/intrepid-sea-air-and-space-museum-new-york",
+            "rating" : "Yelp rating"
+        }
+    },
+      { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [-73.97103, 40.77994]},
+        "properties": {
+
+            'marker-color': '#FFF0AA',
+            'marker-symbol': 'museum',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/natural-musem.jpg",
+            "url": "http://www.amnh.org/",
+            "name": "American Museum of Natural History",
+            "address": "79th Street and Central Park West. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/american-museum-of-natural-history-new-york",
+            "rating" : "Yelp rating"
+        }
+    },
+       { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [-73.987967, 40.719964]},
+        "properties": {
+
+            'marker-color': '#BCF1ED',
+            'marker-symbol': 'shop',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/economy-candy.jpg",
+            "url": "http://www.economycandy.com/",
+            "name": "Economy Candy",
+            "address": "108 Rivington St. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/economy-candy-market-new-york",
+            "rating" : "Yelp rating"
+        }
+    },
+        { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [-73.979302, 40.758005]},
+        "properties": {
+
+            'marker-color': '#BCF1ED',
+            'marker-symbol': 'shop',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/nitendostorefront.jpg",
+            "url": "http://www.nintendoworldstore.com/",
+            "name": "Nintendo World",
+            "address": "10 Rockefeller Plz. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/nintendo-world-store-new-york",
+            "rating" : "Yelp rating"
+        }
+    },
+       { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [-73.977833, 40.758079]},
+        "properties": {
+
+            'marker-color': '#BCF1ED',
+            'marker-symbol': 'shop',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/american_girl_place.jpg",
+            "url": "http://www.americangirl.com/stores/location_ny.php",
+            "name": "American Girl Place",
+            "address": "609 Fifth Avenue &49th St. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/american-girl-place-new-york",
+            "rating" : "Yelp rating"
+        }
+    },
+       { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [-73.979439, 40.75782]},
+        "properties": {
+
+            'marker-color': '#BDF4A0',
+            'marker-symbol': 'bar',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/morrellwine.jpg",
+            "url": "http://morrellwinebar.com/",
+            "name": "Morrell Wine Bar & Cafe",
+            "address": "1 Rockefeller Plaza. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/the-morrell-wine-bar-and-cafe-new-york",
+            "rating" : "Yelp rating"
+        }
+    },
+    { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [-73.989324, 40.76038]},
+        "properties": {
+
+            'marker-color': '#BDF4A0',
+            'marker-symbol': 'bar',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/swing46.jpg",
+            "url": "http://swing46.com/",
+            "name": "Swing 46",
+            "address": "349 W. 46th St. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/swing-46-new-york",
+            "rating" : "Yelp rating"
+        }
+    },
+    { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [ -73.975563, 40.736302]},
+        "properties": {
+
+            'marker-color': '#044C29',
+            'marker-symbol': 'park2',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/park.jpg",
+            "url": "http://www.nycgovparks.org/parks/asserlevy",
+            "name": "Asser Levy Recreation Center",
+            "address": "Asser Levy Place & E 24-25 St. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/asser-levy-recreation-center-new-york",
+            "rating" : "Yelp rating"
+        }
+    },
+     { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [ -73.97767, 40.75212]},
+        "properties": {
+
+            'marker-color': '#C7B773',
+            'marker-symbol': 'rail',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/grand-central-terminal-new-york.jpg",
+            "url": "http://www.grandcentralterminal.com/",
+            "name": "Grand Central Terminal",
+            "address": "42nd Street & Park Avenue. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/grand-central-terminal-new-york",
+            "rating" : "Yelp rating"
+        }
+    },
+        { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [ -74.01429, 40.702875]},
+        "properties": {
+
+            'marker-color': '#266BFA',
+            'marker-symbol': 'monument',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/statue-liberty.jpg",
+            "url": "http://www.nps.gov/stli/index.htm",
+            "name": "Statue of Liberty",
+            "address": "Liberty Island. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/statue-of-liberty-national-monument-new-york#query:Statue%20of%20Libert",
+            "rating" : "Yelp rating"
+        }
+    },
+        { "type": "Feature",
+       "geometry": { "type": "Point", "coordinates": [ -73.978488, 40.759317]},
+        "properties": {
+
+            'marker-color': '#266BFA',
+            'marker-symbol': 'monument',
+            "image": "http://i130.photobucket.com/albums/p277/boklos/map/new%20map/rockefeller-center.jpeg",
+            "url": "http://www.rockefellercenter.com/",
+            "name": "Rockfeller Center",
+            "address": "47th & 51th St. New York City, NY",
+            "yelp": "http://api.yelp.com/v2/business/rockefeller-center-new-york",
+            "rating" : "Yelp rating"
+        }
+    }
+  ]
+}
